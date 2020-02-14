1be71289561781e16c79d5b7373c1578d40cd22c
viagram <viagram@users.noreply.github.com>
2020-02-14 14:35:51 +0800

    Update c.sh

--- c.sh
+++ c.sh
@@ -18,7 +18,9 @@ function install(){
 	zsph="/usr/bin/dnsctl"
 	myph="$(dirname $(readlink -f $0))/$(basename $0)"
 	if [[ ${myph} != ${zsph} ]]; then
-		cp -rf ${myph} ${zsph} && chmod +x ${zsph} && rm -rf ${myph}
+		cp -rf ${myph} ${zsph} >/dev/null 2>&1 && \
+		chmod +x ${zsph} && \
+		rm -rf ${myph}
 	fi
 }
 
